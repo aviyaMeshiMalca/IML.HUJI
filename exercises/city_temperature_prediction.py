56ff7f1541bfff6468bf5b40e9e78f5e19ec3237
AviyaMeshiMalca <aviyamalca@gmail.com>
2023-04-27 19:46:38 +0300

    load data fixing

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -26,29 +26,20 @@ def load_data(filename: str) -> pd.DataFrame:
     Design matrix and response vector (Temp)
     """
     temperature_df = pd.read_csv(filename, parse_dates=['Date'], na_values=['-'])
-
     # Convert the column to float
     temperature_df['Temp'] = pd.to_numeric(temperature_df['Temp'], errors='coerce')
-
-    # # Drop rows with NaN values in the column
-    # temperature_df.dropna(subset=['Temp'], inplace=True)
-
     temperature_df.dropna(inplace=True)
-
     # Add a DayOfYear column
     temperature_df['DayOfYear'] = temperature_df['Date'].dt.dayofyear
-
     #check that there are no null
     # create a boolean mask of null values
     mask = temperature_df.isnull().any(axis=1)
     temperature_df = temperature_df.loc[~mask]
-
     #check that cols which soppused to have 
     # only positive values actually make it
     pos_val_X_cols = ["Year","Month","Day"]
     valid_positive_rows = (temperature_df[pos_val_X_cols] >= 0).all(axis=1)
     temperature_df = temperature_df.loc[valid_positive_rows]
-
     min_temp = -30
     max_temp = 60
     temperature_df = temperature_df[temperature_df["Temp"] >= min_temp]
